92f27a5c9256c9f75ac92f250230afcf29f0abbd
Fabio Siliberto <fabio.siliberto@gmail.com>
2021-03-27 01:18:49 +0100

    Hotfix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://silibcloud-hangman.netlify.app']
+    origin: ['http://localhost:4200', 'https://silibcloud-hangman.netlify.app']
   });
   await app.listen(process.env.PORT || 3000);
 }
