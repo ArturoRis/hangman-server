bfb6a017a44924b670f83e2eb391e165b2bca035
Fabio Siliberto <fabio.siliberto@gmail.com>
2021-03-27 16:57:01 +0100

    Fix cors only on prod

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://silibcloud-hangman.netlify.app']
+    origin: process.env.ENV === 'prod'
+      ? ['https://silibcloud-hangman.netlify.app']
+      : ['http://localhost:4200']
   });
   await app.listen(process.env.PORT || 3000);
 }
